cf85d9f66159409beb2865f7751c313fad2647b7
betaveros <brian.chxn@gmail.com>
2013-04-20 22:45:31 +0800

    Fix TextureFillRectStamp (use a new TexturePaint every time)

--- src/main/scala/gridderface/stamp/TextureFillRectStamp.scala
+++ src/main/scala/gridderface/stamp/TextureFillRectStamp.scala
@@ -8,29 +8,35 @@ import java.awt.TexturePaint
 import java.awt.image.BufferedImage
 import java.awt.Color
 
-class TextureFillRectStamp(val texture: Paint) extends RectStamp {
+class TextureFillRectStamp(val texture: Paint, val textureWidth: Int, val textureHeight: Int) extends RectStamp {
   def drawRect(g2d: Graphics2D, paint: Paint, x: Double, y: Double, w: Double, h: Double) {
-    // note: this requires painting on something with alpha!
+    val prepImg = new BufferedImage(textureWidth, textureHeight, BufferedImage.TYPE_INT_ARGB)
+    val prepRect = new Rectangle2D.Double(0, 0, textureWidth, textureHeight)
+    val prepG = prepImg.createGraphics()
+    prepG setPaint paint
+    prepG fill prepRect
+    prepG setComposite AlphaComposite.DstIn
+    prepG setPaint texture
+    prepG fill prepRect
+
+
     val copy = g2d.create().asInstanceOf[Graphics2D]
-    copy.translate(x, y);
+    copy.translate(x, y)
     val rect = new Rectangle2D.Double(0, 0, w, h)
     
     
-    copy.setPaint(paint);
-    copy.fill(rect);
-    copy.setComposite(AlphaComposite.DstIn);
-    copy.setPaint(texture);
-    copy.fill(rect);
+    copy setPaint (new TexturePaint(prepImg, prepRect))
+    copy fill rect
   }
 }
 
 object TextureFillRectStamp {
   private def makeSimple3x3Stamp(x1: Int, y1: Int, x2: Int, y2: Int) = {
-    val bi = new BufferedImage(3, 3, BufferedImage.TYPE_INT_ARGB);
+    val bi = new BufferedImage(3, 3, BufferedImage.TYPE_INT_ARGB)
     val g2d = bi.createGraphics();
     g2d.setColor(Color.BLACK); // doesn't really matter
     g2d.drawLine(x1, y1, x2, y2);
-    new TextureFillRectStamp(new TexturePaint(bi, new Rectangle2D.Double(0,0,3,3)))
+    new TextureFillRectStamp(new TexturePaint(bi, new Rectangle2D.Double(0,0,3,3)), 3, 3)
   }
   val diagonalStamp = makeSimple3x3Stamp(0, 0, 2, 2)
   val dashedStamp = makeSimple3x3Stamp(0, 0, 1, 0)
