65052d63f6e5c244adb7d8090af36d3d8da487db
Brian Chen <brian.chxn@gmail.com>
2015-01-19 18:04:51 +0800

    Adjust guessed grid if later grid lines look close

--- src/main/scala/gridderface/GridGuesser.scala
+++ src/main/scala/gridderface/GridGuesser.scala
@@ -2,6 +2,7 @@ package gridderface
 
 import java.awt.image.BufferedImage
 import scala.math.{ sqrt, abs, max }
+import scala.collection.mutable.ArrayBuffer
 
 object GridGuesser {
   private def rgb(n: Int) = ((n >> 16) & 0xff, (n >> 8) & 0xff, n & 0xff)
@@ -14,51 +15,69 @@ object GridGuesser {
   private def diffVs(img: BufferedImage, x: Int, y: Int, dx: Int, dy: Int) =
     posDiff(img.getRGB(x, y), img.getRGB(x + dx, y + dy))
 
-  private def help(fxs: IndexedSeq[Double], bxs: IndexedSeq[Double], ft: Double, bt: Double, fsi1: Int, bsi1: Int): Option[(Double, Double)] = {
+  private def help(fxs: IndexedSeq[Double], bxs: IndexedSeq[Double], ft: Double, bt: Double, fsi1: Int, bsi1: Int): Seq[Double] = {
+    // we now suppose each "grid line" starts at a large value from fxs
+    // and ends at a large value from bxs
+    // (it could have been the other way around outside this function)
+    var fsi = fsi1
+    var bsi = bsi1
+    var rxs = ArrayBuffer[Double]()
     val len = fxs.length max bxs.length
 
-    // find start of second line
-    val fsi2 = fxs.indexWhere(_ >= ft, from = bsi1 + 1)
-    if (fsi2 == -1) return None
+    while (true) {
+      // find start of next line
+      val nextfsi = fxs.indexWhere(_ >= ft, from = bsi + 1) match {
+        case -1 => len
+        case x => x
+      }
 
-    // work backwards to probable end of first line
-    val bei1 = bxs.lastIndexWhere(_ >= bt, end = fsi2 - 1)
+      // work backwards to probable end of previous line
+      val bei = bxs.lastIndexWhere(_ >= bt, end = nextfsi - 1)
 
-    // find lower bound for end of second line
-    val bsi2 = bxs.indexWhere(_ >= bt, from = fsi2)
-    if (bsi2 == -1) return None
+      rxs :+= (fsi + bei) / 2.0
 
-    // find start of third line, or just ending boundary
-    val fsi3 = fxs.indexWhere(_ >= ft, from = bsi2 + 1) match { case -1 => len; case x => x }
+      if (nextfsi == len) return rxs.toSeq
 
-    // work backwards to probable end of second line
-    val bei2 = bxs.lastIndexWhere(_ >= bt, end = fsi3 - 1)
+      // find lower bound for end of next line
+      val nextbsi = bxs.indexWhere(_ >= bt, from = nextfsi)
+      if (nextbsi == -1) return rxs.toSeq
 
-    return Some((fsi1 + bei1) / 2.0, (fsi2 + bei2) / 2.0)
+      fsi = nextfsi
+      bsi = nextbsi
+    }
+    throw new AssertionError("what?")
   }
-  private def getTwoCentralIndicesWith(fxs: IndexedSeq[Double], bxs: IndexedSeq[Double], threshold: Double): Option[(Double, Double)] = {
+  private def getCentralIndicesWith(fxs: IndexedSeq[Double], bxs: IndexedSeq[Double], threshold: Double): Seq[Double] = {
     val ft = fxs.max * threshold
     val bt = bxs.max * threshold
     // darn scala why do these return -1 instead of Option[Int]
     val fsi1 = fxs.indexWhere(_ >= ft)
     val bsi1 = bxs.indexWhere(_ >= bt)
-    if (fsi1 == -1 || bsi1 == -1) None
+    if (fsi1 == -1 || bsi1 == -1) Seq()
     else if (fsi1 < bsi1) help(fxs, bxs, ft, bt, fsi1, bsi1)
     else help(bxs, fxs, bt, ft, bsi1, fsi1)
   }
-
-  private def getTwoIndices(fxs: IndexedSeq[Double], bxs: IndexedSeq[Double]): (Double, Double) = {
+  private def regress(s: Seq[Double]): (Double, Double) = {
+    var skip = s(1) - s(0)
+    for (i <- 2 until s.length) {
+      val s2 = (s(i) - s(0)) / i
+      if (abs(s2 - skip) < 1) skip = s2
+    }
+    return (s(0), skip)
+  }
+  private def getStartStep(fxs: IndexedSeq[Double], bxs: IndexedSeq[Double]): (Double, Double) = {
     var threshold = 1.0 / 3.0
 
     for (i <- 1 to 20) {
-      getTwoCentralIndicesWith(fxs, bxs, threshold) match {
-        case Some(res) => return res
-        case None => threshold *= 0.75
-      }
+      val res = getCentralIndicesWith(fxs, bxs, threshold)
+      if (res.length > 1) return regress(res)
+      threshold *= 0.75
     }
     return (0, fxs.length - 1) // give up
   }
   def guess(img: BufferedImage): SimpleGrid = {
+    // calculate "total difference" betwen every two adjacent lines of pixels
+    // always positive.
     val yForwardDiffs = for (y <- 0 until (img.getHeight - 1)) yield
       (0 until img.getWidth map (diffVs(img, _, y, 0, 1))).sum
     val yBackwardDiffs = for (y <- 1 until img.getHeight) yield
@@ -68,8 +87,8 @@ object GridGuesser {
     val xBackwardDiffs = for (x <- 1 until img.getWidth) yield
       (0 until img.getHeight map (diffVs(img, x, _, -1, 0))).sum
 
-    val (y1, y2) = getTwoIndices(yForwardDiffs, yBackwardDiffs)
-    val (x1, x2) = getTwoIndices(xForwardDiffs, xBackwardDiffs)
+    val (y1, yd) = getStartStep(yForwardDiffs, yBackwardDiffs)
+    val (x1, xd) = getStartStep(xForwardDiffs, xBackwardDiffs)
     // println("yForwardDiffs:")
     // yForwardDiffs.zipWithIndex foreach println
     // println("yBackwardDiffs:")
@@ -79,6 +98,6 @@ object GridGuesser {
     // xForwardDiffs.zipWithIndex foreach println
     // println("xBackwardDiffs:")
     // xBackwardDiffs.zipWithIndex foreach println
-    new SimpleGrid(y2 - y1, x2 - x1, x1 + 1, y1 + 1)
+    new SimpleGrid(yd, xd, x1 + 1, y1 + 1)
   }
 }
