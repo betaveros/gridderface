db7b674181e29665fc1e4e99b8361f27a7446150
Brian Chen <brian.chxn@gmail.com>
2015-04-15 17:04:19 +0800

    Make PaintSet case class; lighter/darker paints

--- src/main/scala/gridderface/PaintSet.scala
+++ src/main/scala/gridderface/PaintSet.scala
@@ -6,41 +6,56 @@ import scala.swing.event.KeyEvent
 import scala.swing.event.KeyTyped
 import scala.collection.immutable.HashMap
 
-class PaintSet(val name: String, val paint: Paint) {}
+case class PaintSet(name: String, paint: Paint) {
+  def darkerSet = paint match {
+    case c: Color => Some(PaintSet("Dark " ++ name, new Color(
+      c.getRed / 2,
+      c.getGreen / 2,
+      c.getBlue / 2)))
+    case _ => None
+  }
+  def lighterSet = paint match {
+    case c: Color => Some(PaintSet("Light " ++ name, new Color(
+      (c.getRed + 255) / 2,
+      (c.getGreen + 255) / 2,
+      (c.getBlue + 255) / 2)))
+    case _ => None
+  }
+}
 
 object PaintSet {
   def createColorSet(color: Color) =
     new PaintSet("#%06X".format(color.getRGB() & 0xffffff), color)
 
-  val blackSet        = new PaintSet("Black",        new Color(  0,  0,  0))
-  val redSet          = new PaintSet("Red",          new Color(255,  0,  0))
-  val greenSet        = new PaintSet("Green",        new Color(  0,224,  0))
-  val blueSet         = new PaintSet("Blue",         new Color(  0,  0,255))
-  val cyanSet         = new PaintSet("Cyan",         new Color(  0,224,224))
-  val magentaSet      = new PaintSet("Magenta",      new Color(224,  0,224))
-  val yellowSet       = new PaintSet("Yellow",       new Color(224,224,  0))
-  val graySet         = new PaintSet("Gray",         new Color(128,128,128))
-  val whiteSet        = new PaintSet("White",        new Color(255,255,255))
+  val blackSet        = PaintSet("Black",        new Color(  0,  0,  0))
+  val redSet          = PaintSet("Red",          new Color(255,  0,  0))
+  val greenSet        = PaintSet("Green",        new Color(  0,224,  0))
+  val blueSet         = PaintSet("Blue",         new Color(  0,  0,255))
+  val cyanSet         = PaintSet("Cyan",         new Color(  0,224,224))
+  val magentaSet      = PaintSet("Magenta",      new Color(224,  0,224))
+  val yellowSet       = PaintSet("Yellow",       new Color(224,224,  0))
+  val graySet         = PaintSet("Gray",         new Color(128,128,128))
+  val whiteSet        = PaintSet("White",        new Color(255,255,255))
 
-  val lightGraySet    = new PaintSet("LightGray",    new Color(192,192,192))
-  val lightRedSet     = new PaintSet("LightRed",     new Color(255,192,192))
-  val lightGreenSet   = new PaintSet("LightGreen",   new Color(128,255,128))
-  val lightBlueSet    = new PaintSet("LightBlue",    new Color(192,192,255))
-  val lightCyanSet    = new PaintSet("LightCyan",    new Color(128,255,255))
-  val lightMagentaSet = new PaintSet("LightMagenta", new Color(255,128,255))
-  val lightYellowSet  = new PaintSet("LightYellow",  new Color(255,255,128))
+  val lightGraySet    = PaintSet("LightGray",    new Color(192,192,192))
+  val lightRedSet     = PaintSet("LightRed",     new Color(255,192,192))
+  val lightGreenSet   = PaintSet("LightGreen",   new Color(128,255,128))
+  val lightBlueSet    = PaintSet("LightBlue",    new Color(192,192,255))
+  val lightCyanSet    = PaintSet("LightCyan",    new Color(128,255,255))
+  val lightMagentaSet = PaintSet("LightMagenta", new Color(255,128,255))
+  val lightYellowSet  = PaintSet("LightYellow",  new Color(255,255,128))
 
-  val azureSet        = new PaintSet("Azure",        new Color(  0,128,255))
-  val emeraldSet      = new PaintSet("Emerald",      new Color(102,204,153))
-  val fireEngineSet   = new PaintSet("FireEngine",   new Color(204, 51, 51))
-  val indigoSet       = new PaintSet("Indigo",       new Color( 96,  0,224))
-  val lemonSet        = new PaintSet("Lemon",        new Color(128,224,  0))
-  val orangeSet       = new PaintSet("Orange",       new Color(255,128,  0))
-  val skyBlueSet      = new PaintSet("SkyBlue",      new Color(153,204,255))
-  val ultramarineSet  = new PaintSet("Ultramarine",  new Color( 63,  0,255))
-  val violetSet       = new PaintSet("Violet",       new Color(128,  0,255))
+  val azureSet        = PaintSet("Azure",        new Color(  0,128,255))
+  val emeraldSet      = PaintSet("Emerald",      new Color(102,204,153))
+  val fireEngineSet   = PaintSet("FireEngine",   new Color(204, 51, 51))
+  val indigoSet       = PaintSet("Indigo",       new Color( 96,  0,224))
+  val lemonSet        = PaintSet("Lemon",        new Color(128,224,  0))
+  val orangeSet       = PaintSet("Orange",       new Color(255,128,  0))
+  val skyBlueSet      = PaintSet("SkyBlue",      new Color(153,204,255))
+  val ultramarineSet  = PaintSet("Ultramarine",  new Color( 63,  0,255))
+  val violetSet       = PaintSet("Violet",       new Color(128,  0,255))
 
-  val devRedSet       = new PaintSet("DevRed",       new Color(204,  0,  0))
+  val devRedSet       = PaintSet("DevRed",       new Color(204,  0,  0))
   // d3.js ordinal categorical colors
   val d3Map: Map[KeyData, PaintSet] = List(
     new Color(31,119,180),
@@ -57,7 +72,7 @@ object PaintSet {
     .zipWithIndex
     .map({case (c, i) => (
       KeyTypedData(('0' + (i + 1) % 10).toChar)
-      -> new PaintSet("d3/" ++ (i + 1).toString, c))})
+      -> PaintSet("d3/" ++ (i + 1).toString, c))})
     .toMap
 
   val basicMap: HashMap[KeyData, PaintSet] = HashMap(
